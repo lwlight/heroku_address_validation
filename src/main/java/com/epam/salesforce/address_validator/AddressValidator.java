6235036f9a86d925593a6d8d266ea41e96df9e8f
Artsiom Kniha <Artsiom_Kniha@epam.com>
2018-09-05 17:29:20 +0300

    db attempt

--- src/main/java/com/epam/salesforce/address_validator/AddressValidator.java
+++ src/main/java/com/epam/salesforce/address_validator/AddressValidator.java
@@ -1,7 +1,12 @@
 package com.epam.salesforce.address_validator;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,6 +14,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -16,6 +27,12 @@ import java.util.Set;
 @RequestMapping("/")
 @SpringBootApplication
 public class AddressValidator {
+
+    @Value("${spring.datasource.url}")
+    private String dbUrl;
+
+    @Autowired
+    private DataSource dataSource;
 
     private static final Set<String> restrictedAddresses = new HashSet<>(3);
     static{
@@ -28,12 +45,44 @@ public class AddressValidator {
         SpringApplication.run(AddressValidator.class, args);
     }
 
-    @RequestMapping(value = "/{address}", method = RequestMethod.GET)
+/*    @RequestMapping(value = "/{address}", method = RequestMethod.GET)
     ResponseEntity<String> address(@PathVariable String address){
         if(restrictedAddresses.contains(address)){
             return new ResponseEntity<>(HttpStatus.FORBIDDEN);
         } else {
             return new ResponseEntity<>(HttpStatus.OK);
+        }
+    }*/
+    @RequestMapping(value = "/{address}", method = RequestMethod.GET)
+    ResponseEntity<String> address(@PathVariable String address){
+        System.out.println("DB URL :: " + dbUrl);
+        try(Connection connection = dataSource.getConnection();){
+            Statement statement = connection.createStatement();
+//            statement.executeUpdate("CREATE TABLE IF NOT EXISTS Rejected_Address (address varchar(50))");
+
+            ResultSet resultSet = statement.executeQuery("SELECT address FROM Rejected_Address WHERE address=" + address);
+            if (resultSet.next()){
+                return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+            } else {
+                return new ResponseEntity<>(HttpStatus.OK);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>("Something went wrong", HttpStatus.BAD_REQUEST);
+    }
+
+
+    @Bean
+    public DataSource dataSource() throws SQLException {
+        System.out.println("DB URL in dataSource :: " + dbUrl);
+        if (dbUrl == null || dbUrl.isEmpty()) {
+            return new HikariDataSource();
+        } else {
+            HikariConfig config = new HikariConfig();
+            config.setJdbcUrl(dbUrl);
+            return new HikariDataSource(config);
         }
     }
 }
