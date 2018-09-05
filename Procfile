1e4d0697ce4be6fba3685f222ea3ded7b87f011b
Artsiom Kniha <Artsiom_Kniha@epam.com>
2018-09-05 14:23:47 +0300

    init commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java -jar target/address-validation-service.jar
---
+++ src/main/java/com/epam/salesforce/address_validator/AddressValidator.java
@@ -0,0 +1,69 @@
+package com.epam.salesforce.address_validator;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.sql.DataSource;
+import javax.websocket.server.PathParam;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+@RestController
+@RequestMapping("/")
+@SpringBootApplication
+public class AddressValidator {
+
+    @Value("${spring.datasource.url}")
+    private String dbUrl;
+
+    @Autowired
+    private DataSource dataSource;
+
+    public static void main(String[] args) {
+        SpringApplication.run(AddressValidator.class, args);
+    }
+
+    @RequestMapping(value = "/{address}", method = RequestMethod.GET)
+    ResponseEntity<String> address(@PathParam("address") String address){
+
+        try (Connection connection = dataSource.getConnection()) {
+            Statement statement = connection.createStatement();
+            statement.executeUpdate("CREATE TABLE IF NOT EXISTS Rejected_Address (address varchar(50))");
+            statement.executeUpdate("INSERT INTO Rejected_Address VALUES ('reject')");
+            ResultSet rs = statement.executeQuery("SELECT address FROM Rejected_Address WHERE address =:address");
+
+            if (rs.next()){
+                return new ResponseEntity<String>(HttpStatus.FORBIDDEN);
+            } else {
+                return new ResponseEntity<String>(HttpStatus.OK);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Bean
+    public DataSource dataSource() throws SQLException {
+        if (dbUrl == null || dbUrl.isEmpty()) {
+            return new HikariDataSource();
+        } else {
+            HikariConfig config = new HikariConfig();
+            config.setJdbcUrl(dbUrl);
+            return new HikariDataSource(config);
+        }
+    }
+}
